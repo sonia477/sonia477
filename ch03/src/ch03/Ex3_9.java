b821c5ff375dd4d91de673284462a25c8937b493
sonia477 <sonia47744@gmail.com>
2022-01-15 12:35:01 +0900

    First Commit

---
+++ ch03/src/ch03/Ex3_9.java
@@ -0,0 +1,16 @@
+package ch03;
+
+public class Ex3_9 {
+
+	public static void main(String[] args) {
+		int a = 1_000_000;  // 1,000,000    1백만 = 10의 6제곱
+		int b = 2_000_000;  // 2,000,000    2백만 = 10의 6제곱
+		
+//		long c = a * b;     // a * b = 2,000,000,000,000 ? 이 아니라 -1454759936이 결과로 나옴
+//		//오버플로우 발생 -> int의 범위는 10의 9제곱
+//		System.out.println(c);
+		long c = (long)a * b;
+		System.out.println(c);
+	}
+
+}
