ac525d83e69c5a6cff3305fed979f3641c22a651
sonia477 <sonia47744@gmail.com>
2022-01-15 12:47:49 +0900

    Second Commit

--- ch03/src/ch03/Ex3_11.java
+++ ch03/src/ch03/Ex3_11.java
@@ -5,7 +5,11 @@ public class Ex3_11 {
 	public static void main(String[] args) {
 		double pi = 3.141592;
 		double shortpi = Math.round(pi*1000)/1000.0;
-		System.out.println(shortpi);
+		System.out.println(shortpi);  // 3.142
+		
+		// 3.141을 얻으려면?
+		
+		
 	}
 
 }
