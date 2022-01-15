c4ac9883efe76e7900c34ef66ac1056ce3b6a6b0
sonia477 <sonia47744@gmail.com>
2022-01-15 16:05:31 +0900

    Second Commit

--- ch03/src/ch03/Ex3_11.java
+++ ch03/src/ch03/Ex3_11.java
@@ -3,13 +3,18 @@ package ch03;
 public class Ex3_11 {
 
 	public static void main(String[] args) {
-		double pi = 3.141592;
-		double shortpi = Math.round(pi*1000)/1000.0;
-		System.out.println(shortpi);  // 3.142
+//		double pi = 3.141592;
+//		double shortpi = Math.round(pi*1000)/1000.0;
+//		System.out.println(shortpi);  // 3.142
 		
 		// 3.141을 얻으려면?
+//		double pi = 3.141592;
+//		double pi2 = pi*1000;
+//		double shortpi = (int)pi2/1000.0;
+//		System.out.println(shortpi);
 		
-		
+		double pi = 3.141592;
+		System.out.println((int)(pi*1000)/1000.0);
 	}
 
 }
