355fccc4167bcf42a9319fc1eae54e454dc59e9d
sonia477 <sonia47744@gmail.com>
2022-01-17 23:52:06 +0900

    First Commit

---
+++ ch04/src/Ex4_12.java
@@ -0,0 +1,24 @@
+
+public class Ex4_12 {
+
+	public static void main(String[] args) {
+//		System.out.println("**********");
+//		System.out.println("**********");
+//		System.out.println("**********");
+//		System.out.println("**********");
+//		System.out.println("**********");
+		
+//		for (int i=1; i<=5; i++) {
+//			System.out.println("**********");
+//		}
+		
+		for (int i=1; i<=5; i++) {
+			for (int j=1; j<=10; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
