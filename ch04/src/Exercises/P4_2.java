2508635079795a82fbf4b33635bf7ca80e0bbf7a
sonia477 <sonia47744@gmail.com>
2022-01-18 19:05:47 +0900

    First Commit

---
+++ ch04/src/Exercises/P4_2.java
@@ -0,0 +1,29 @@
+package Exercises;
+
+public class P4_2 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for (int i=1; i<=20; i++) {
+			if (i%2==0 || i%3==0) {
+				continue;
+			}
+			sum += i;
+//			System.out.println("i =" + i + ", sum =" + sum);
+		}
+		
+			
+			
+//		int sum = 0;
+//		
+//		for (int i=1; i<=20; i++) {
+//			if (i%2!=0 && i%3!=0) {
+//				sum += i;
+//			}
+//		}
+		
+		System.out.println("sum=" + sum);
+
+		
+	}
+}
