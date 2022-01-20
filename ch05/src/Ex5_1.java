e26a56c2dc91efa626b0994ca97e189617575e84
sonia477 <sonia47744@gmail.com>
2022-01-20 18:30:48 +0900

    Second Commit

--- ch05/src/Ex5_1.java
+++ ch05/src/Ex5_1.java
@@ -20,7 +20,7 @@ public class Ex5_1 {
 		int[] arr = new int[10];
 		System.out.println("arr.length="+arr.length);
 		
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<arr.length; i++) {
 			System.out.println("arr[" + i + "]=" + arr[i]);
 		}
 		
